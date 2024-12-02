8b6ce76f7a113261ee6b800bc3a8c1a91b33be1e
SkwalExe <skwal@skwal.net>
2024-12-02 19:35:25 +0100

    Formatting

--- src/commands/click_mode.rs
+++ src/commands/click_mode.rs
@@ -1,16 +1,13 @@
 use super::Command;
-use crate::{
-    app_state::ClickMode,
-    AppState,
-};
+use crate::{app_state::ClickMode, AppState};
 
 pub(crate) fn execute_click_mode(state: &mut AppState, args: Vec<&str>) -> Result<(), String> {
     if args.is_empty() {
         state.log_raw(format!(
             "Current click configuration:\nLeft: <acc {}>\nMiddle: <acc {}>\nRight: <acc {}>\nAvailable click actions:\n{}",
-            state.click_config.left, 
-            state.click_config.middle, 
-            state.click_config.right, 
+            state.click_config.left,
+            state.click_config.middle,
+            state.click_config.right,
             ClickMode::all().iter().map(|x| format!("<acc {x}>")).collect::<Vec<String>>().join(", ")
         ));
         return Ok(());
@@ -24,9 +21,12 @@ pub(crate) fn execute_click_mode(state: &mut AppState, args: Vec<&str>) -> Resul
         "left" => &mut state.click_config.left,
         "middle" => &mut state.click_config.middle,
         "right" => &mut state.click_config.right,
-        _ => return Err("The given button identifier could not be interpreted".to_string())
+        _ => return Err("The given button identifier could not be interpreted".to_string()),
     } = action.clone();
-    state.log_success(format!("Successfully configured <acc {}> click to trigger <acc {}>", button_str, action));
+    state.log_success(format!(
+        "Successfully configured <acc {}> click to trigger <acc {}>",
+        button_str, action
+    ));
 
     Ok(())
 }
