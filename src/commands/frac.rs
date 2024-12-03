5a8c1d5819fa2784cf3f2321def06ee84ed86d72
SkwalExe <skwal@skwal.net>
2024-12-03 09:37:35 +0100

    Start adding some tests

--- src/commands/frac.rs
+++ src/commands/frac.rs
@@ -65,3 +65,34 @@ pub(crate) const FRAC: Command = Command {
     )),
     basic_desc: "List available fractals, and provide info about them.",
 };
+
+#[cfg(test)]
+mod tests {
+    use crate::AppState;
+
+    use super::execute_frac;
+
+    #[test]
+    fn test_frac_command() {
+        let mut state = AppState::default();
+        // `frac ju` should return Ok and the Julia fracal should be selected
+        execute_frac(&mut state, vec!["ju"]).unwrap();
+        assert_eq!(state.render_settings.get_frac_obj().name, "Julia");
+
+        // `frac non_exist` should return Err
+        assert!(execute_frac(&mut state, vec!["non_exist"]).is_err());
+
+        // `frac info mandel` should return Ok and a log message should have been added
+        let count_before = state.log_messages.len();
+        execute_frac(&mut state, vec!["info", "mandel"]).unwrap();
+        assert_eq!(state.log_messages.len(), count_before + 1);
+        //
+        // `frac` should return Ok and a log message should have been added
+        let count_before = state.log_messages.len();
+        execute_frac(&mut state, vec![]).unwrap();
+        assert_eq!(state.log_messages.len(), count_before + 1);
+
+        // `frac info non_exist` should return Err
+        assert!(execute_frac(&mut state, vec!["info", "non_exist"]).is_err());
+    }
+}
