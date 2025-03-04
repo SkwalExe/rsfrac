a97311ffc5a3ad5fa4c45aa4c11d51c4641e44a8
SkwalExe <skwal@skwal.net>
2025-03-04 10:44:07 +0100

    Allow to specify filename when making screenshot

--- src/commands/capture_hq.rs
+++ src/commands/capture_hq.rs
@@ -1,18 +1,29 @@
 use super::{capture::execute_capture, Command};
 use crate::AppState;
 
-pub(crate) fn execute_capture_fit(state: &mut AppState, _args: Vec<&str>) -> Result<(), String> {
+pub(crate) fn execute_capture_fit(state: &mut AppState, args: Vec<&str>) -> Result<(), String> {
     let scale = 4;
     let width = 1920 * scale;
     let height = 1080 * scale;
-    execute_capture(state, vec![&format!("{width}"), &format!("{height}")])
+    let width_str = format!("{width}");
+    let height_str = format!("{height}");
+    let mut capture_args: Vec<&str> = vec![&width_str, &height_str];
+
+    if args.len() == 1 {
+        capture_args.push(args[0]);
+    }
+
+    execute_capture(state, capture_args)
 }
 
 pub(crate) const CAPTURE_HQ: Command = Command {
     execute: &execute_capture_fit,
     name: "capture_hq",
     aliases: &["chq"],
-    accepted_arg_count: &[0],
+    accepted_arg_count: &[0, 1],
     detailed_desc: None,
-    basic_desc: "Takes a <acc VERY> high quality screenshot.",
+    basic_desc: concat!(
+        "Takes a <acc VERY> high quality screenshot. ",
+        "You can optionnaly provide a name for the screenshot."
+    ),
 };
