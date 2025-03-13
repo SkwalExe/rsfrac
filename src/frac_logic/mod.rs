01c56c1b7909b52939635f34aa426dff5d9fe051
SkwalExe <skwal@skwal.net>
2025-03-13 21:00:49 +0100

    format

--- src/frac_logic/mod.rs
+++ src/frac_logic/mod.rs
@@ -2,17 +2,17 @@
 
 mod canvas_coords;
 mod fractal_logic;
-mod gpu_rendering_tracker;
-mod wgpu_state;
-mod render_settings_methods;
-mod params_binding;
 pub(crate) mod gpu_init;
 pub(crate) mod gpu_render;
+mod gpu_rendering_tracker;
 pub(crate) mod gpu_util;
+mod params_binding;
 mod render_settings;
+mod render_settings_methods;
+mod wgpu_state;
 
 pub(crate) use canvas_coords::CanvasCoords;
-pub(crate) use wgpu_state::WgpuState;
-pub(crate) use params_binding::ParamsBinding;
 pub(crate) use fractal_logic::DivergMatrix;
+pub(crate) use params_binding::ParamsBinding;
 pub(crate) use render_settings::RenderSettings;
+pub(crate) use wgpu_state::WgpuState;
